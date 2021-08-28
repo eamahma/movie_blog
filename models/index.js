f89a54cc12bdb89a19f1b964dc44ee1a93669566
eamahma <eamahma@gmail.com>
2021-08-28 14:13:03 -0400

    MVC folders added

---
+++ models/index.js
@@ -0,0 +1,13 @@
+const User = require('./User');
+const Note = require('./Note');
+
+User.hasMany(Note, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+Note.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+module.exports = { User, Note };
