634dddbb2143deffdab571d343578342f53d6ff4
eamahma <eamahma@gmail.com>
2021-08-31 20:03:25 -0400

    Genre model created

---
+++ models/Genre.js
@@ -0,0 +1,46 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Genre extends Model {}
+
+Note.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
+    note_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'note',
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'genre',
+  }
+);
+
+module.exports = Genre;
