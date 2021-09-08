6b3b588bbe055278fc54057cd2cb993dcdac783d
eamahma <eamahma@gmail.com>
2021-09-08 05:29:54 -0400

    homepage styling with pure

--- models/Post.js
+++ models/Post.js
@@ -23,7 +23,11 @@ Post.init(
       type: DataTypes.TEXT,
       allowNull: false,
     },
- 
+    date: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW
+    },
     user_id: {
       type: DataTypes.INTEGER,
       references: {
